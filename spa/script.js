a4b856fc21a6e38680813a839e86c8a0874b9d97
Pedro Henrique <pedrohensantosaraujo@gmail.com>
2023-02-02 18:08:14 -0300

    update

--- spa/script.js
+++ spa/script.js
@@ -6,7 +6,6 @@ router.add('/about', './pages/about.html')
 router.add('/contact','./pages/contact.html')
 router.add(404,'./pages/404.html')
 
-
 router.handle()
 
 window.onpopstate = () => router.handle()
